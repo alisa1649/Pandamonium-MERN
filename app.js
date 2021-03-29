285a9cd8b1f48c52cdb0eb42d9b9fb5b7edf2278
alisa1649 <alisa1649@gmail.com>
2021-03-29 14:36:15 -0400

    Added keys files to config

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const mongoose = require('mongoose');
 const express = require("express");
 const app = express();
+const path = require('path');
 const bodyParser = require('body-parser');
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
 const passport = require('passport');
+
+
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
   .catch(err => console.log(err));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 
 app.use(passport.initialize());
