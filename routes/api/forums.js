3b2c70dee0ebde4920ef7216421694af4aa29ba2
Danny <dannyho77@gmail.com>
2021-03-31 09:07:17 -0400

    revised some routes on posts and forums

--- routes/api/forums.js
+++ routes/api/forums.js
@@ -5,13 +5,13 @@ const mongoose = require('mongoose');
 const Forum = require('../../models/Forum');
 
 
-router.get('/forums/:forum_id', (req, res) => {
+router.get('/:forum_id', (req, res) => {
     Forum.find({forum: req.params.forum_id})
         .then(forum => res.json(forum))
         .catch(err => res.status(404).json({ noforumfound: 'This forum does not exist.' }));
 });
 
-router.post('/forums/new', (req, res) => {
+router.post('/new', (req, res) => {
   
       const newForum = new Forum({
         name: req.body.name,
