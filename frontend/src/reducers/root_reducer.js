b1777fa8e4643c047cca760508f52ac470207bc5
alisa1649 <alisa1649@gmail.com>
2021-03-31 09:52:55 -0400

    Resolved one more merge conflict

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux';
 import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer'
 import EntitiesReducer from './entities_reducer';
-import PostsReducer from './posts_reducer'
 import ParentPostsReducer from './parent_posts_reducer'
 import ThreadReducer from "./thread_reducer";
 import uiReducer from './ui_reducer';
