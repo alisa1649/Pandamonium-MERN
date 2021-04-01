d5d6e7232fc17d6dc38576345afba191b8028ac0
Lane Harris <lrharris215@gmail.com>
2021-04-01 09:41:54 -0400

    Adds other-user actions and user util

--- frontend/src/actions/user_actions.js
+++ frontend/src/actions/user_actions.js
@@ -1,11 +1,17 @@
 import * as APIUtil from '../util/user_util';
 
 export const RECEIVE_CURRENT_USER_INFO = "RECEIVE_CURRENT_USER_INFO";
+export const RECEIVE_OTHER_USER_INFO = "RECEIVE_OTHER_USER_INFO"
 
 export const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER_INFO,
     currentUser
 });
+
+export const receiveOtherUser = user => ({
+    type: RECEIVE_OTHER_USER_INFO,
+    user,
+})
 
 export const getCurrentUserInfo = () => dispatch => {
     return (
@@ -18,3 +24,10 @@ export const editCurrentUserInfo = (newInfo) => dispatch => {
         APIUtil.editCurrentUserInfo(newInfo).then((currentUser) => dispatch(receiveCurrentUser(currentUser)))
     )
 }
+
+export const getOtherUserInfo = () => dispatch => {
+    return (
+        APIUtil.getOtherUserInfo().then((otherUser) => dispatch(receiveOtherUser(otherUser)))
+    )
+}
+
