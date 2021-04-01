28288c08432a09a41f5b81982265e178d7922dff
Lane Harris <lrharris215@gmail.com>
2021-03-31 21:18:56 -0400

    some styling on nav-bar

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -16,19 +16,19 @@ class NavBar extends React.Component {
     getLinks() {
         if (this.props.loggedIn) {
             return (
-                <div>
+                <div className="links">
                     <Link to={'/'}>Home</Link>
                     <Link to={'/profile'}>Profile</Link>
                     {/* <Link to={'/new_tweet'}>Write a Tweet</Link> */}
                     <button onClick={this.logoutUser}>Logout</button>
                 </div>
             );
-        } 
+        }
     }
 
     render() {
         return (
-            <div>
+            <div className="nav-bar">
                 <h1>Pandamonium</h1>
                 {this.getLinks()}
             </div>
