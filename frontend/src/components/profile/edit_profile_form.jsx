2a0d0709bce0092c6f212a9c321b68dd3de6e3ee
yohan oh <yohan.oh332@gmail.com>
2021-04-01 17:21:01 -0400

    merging

--- frontend/src/components/profile/edit_profile_form.jsx
+++ frontend/src/components/profile/edit_profile_form.jsx
@@ -103,9 +103,6 @@ class EditProfileForm extends React.Component {
         });
     }
 
-    handleChange= (event) => {
-        this.setState({[event.target.name]: event.target.value})
-    }
 
     render() {
         if (this.state.redirect === true) {
