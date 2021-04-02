e8426722cbfa1d00edf974fc76d3a6f1bc9add58
Lane Harris <lrharris215@gmail.com>
2021-04-01 20:58:47 -0400

    remembered to save thread in components folder

--- frontend/src/components/thread/post/thread.jsx
+++ frontend/src/components/thread/post/thread.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { connect } from "react-redux";
-import { Link, Redirect } from "react-router-dom";
-import {createComment, deleteComment, requestThread} from "../../actions/thread_actions";
-import NewPostForm from "../../components/dashboard/new_post_form";
-import PostListItem from "../../components/dashboard/post_list_item";
-import EditPostModal from "../../components/dashboard/edit_post_modal";
-import {updateParentPost} from "../../actions/parent_post_actions";
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import { createComment, deleteComment, requestThread } from '../../../actions/thread_actions';
+import NewPostForm from '../../dashboard/new_post_form';
+import PostListItem from '../../dashboard/post_list_item';
+import EditPostModal from '../../dashboard/edit_post_modal';
+import { updateParentPost } from '../../../actions/parent_post_actions';
 
 class Thread extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            editModalVisible: false
-        }
+            editModalVisible: false,
+        };
     }
 
     componentDidMount() {
@@ -28,35 +28,42 @@ class Thread extends React.Component {
             comment.parent = this.props.parentPost._id;
             comment.user = this.props.userId;
             this.props.createComment(comment);
-        }
+        };
         if (this.props.parentPost) {
             return (
                 <div>
-                    {
-                        this.state.editModalVisible
-                            ? <EditPostModal
-                                post={this.props.parentPost}
-                                closeAction={() => this.setState({editModalVisible: false} )}
-                                submitAction={(post) => this.props.updateParentPost(post)} />
-                            : ""
-                    }
-                    <NewPostForm createPost={createComment}   />
+                    {this.state.editModalVisible ? (
+                        <EditPostModal
+                            post={this.props.parentPost}
+                            closeAction={() => this.setState({ editModalVisible: false })}
+                            submitAction={(post) => this.props.updateParentPost(post)}
+                        />
+                    ) : (
+                        ''
+                    )}
+                    <NewPostForm createPost={createComment} />
                     <div>
-                        <Link to='/dashboard'>Back to Dashboard</Link>
+                        <Link to="/dashboard">Back to Dashboard</Link>
                     </div>
-                    <PostListItem post={this.props.parentPost} editAction={() => this.setState({editModalVisible: true})}/>
-                    <ul className='post-list'>
-                        {
-                            this.props.comments.map(comment => {
-                                return <PostListItem key={comment._id} post={comment} deleteAction={this.props.deleteComment} />
-                            })
-                        }
+                    <PostListItem
+                        post={this.props.parentPost}
+                        editAction={() => this.setState({ editModalVisible: true })}
+                    />
+                    <ul className="post-list">
+                        {this.props.comments.map((comment) => {
+                            return (
+                                <PostListItem
+                                    key={comment._id}
+                                    post={comment}
+                                    deleteAction={this.props.deleteComment}
+                                />
+                            );
+                        })}
                     </ul>
                 </div>
             );
-        }
-        else {
-            return <Redirect to='/dashboard' />
+        } else {
+            return <Redirect to="/dashboard" />;
         }
     }
 }
@@ -64,15 +71,13 @@ class Thread extends React.Component {
 const mapStateToProps = (state, ownProps) => ({
     userId: state.session.user.id,
     parentPost: state.entities.parent_posts[ownProps.match.params.postId],
-    comments: Object.keys(state.entities.thread).length
-        ? Object.values(state.entities.thread.comments)
-        : []
+    comments: Object.keys(state.entities.thread).length ? Object.values(state.entities.thread.comments) : [],
 });
 
 const mapDispatchToProps = (dispatch) => ({
     updateParentPost: (post) => dispatch(updateParentPost(post)),
     createComment: (comment) => dispatch(createComment(comment)),
     requestThread: (postId) => dispatch(requestThread(postId)),
-    deleteComment: (postId) => dispatch(deleteComment(postId))
-})
+    deleteComment: (postId) => dispatch(deleteComment(postId)),
+});
 export default connect(mapStateToProps, mapDispatchToProps)(Thread);
