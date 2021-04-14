8d19a05d44922ae455df7a77566b14126f95d644
Lane Harris <lrharris215@gmail.com>
2021-04-14 12:12:42 -0400

    Added Alisa's and Lane's info

--- frontend/src/components/about_page/about_us.js
+++ frontend/src/components/about_page/about_us.js
@@ -9,9 +9,11 @@ const AboutUs = (props) => {
                 </p>
             </div>
 
-            <div className="about-us-item">
+            <div id="alisa" className="about-us-item">
                 <div className="left">
-                    <div className="about-us-pic"></div>
+                    <div className="about-us-pic">
+                        <img src="https://avatars.githubusercontent.com/u/74744805?v=4"></img>
+                    </div>
                     <div className="about-us-info">
                         <h3 className="name">
                             Alisa Cheadle- <i>Role</i>
@@ -20,12 +22,18 @@ const AboutUs = (props) => {
                     </div>
                 </div>
                 <div className="link-box">
-                    <i className="fab fa-github"></i>
-                    <i className="fab fa-linkedin"></i>
-                    <i className="fas fa-user-circle"></i>
+                    <a href="https://github.com/alisa1649" target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/alisa-cheadle-04a7811a7/" target="_blank">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    <a href="http://alisacheadle.com/" target="_blank">
+                        <i className="fas fa-user-circle"></i>
+                    </a>
                 </div>
             </div>
-            <div className="about-us-item">
+            <div id="danny" className="about-us-item">
                 <div className="left">
                     <div className="about-us-pic"></div>
                     <div className="about-us-info">
@@ -36,14 +44,25 @@ const AboutUs = (props) => {
                     </div>
                 </div>
                 <div className="link-box">
-                    <i className="fab fa-github"></i>
-                    <i className="fab fa-linkedin"></i>
-                    <i className="fas fa-user-circle"></i>
+                    <a href="" target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="" target="_blank">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    {/* personal site goes here */}
+                    <a href="" target="_blank">
+                        <i className="fas fa-user-circle"></i>
+                    </a>
                 </div>
             </div>
-            <div className="about-us-item">
+            <div id="lane" className="about-us-item">
                 <div className="left">
-                    <div className="about-us-pic"></div>
+                    <div className="about-us-pic">
+                        <img
+                            id="lane-pic"
+                            src="https://s3-us-west-2.amazonaws.com/aa-progress-tracker/students/avatars/000/005/906/medium/Lane_Harris.jpg?1610668094"></img>
+                    </div>
                     <div className="about-us-info">
                         <h3 className="name">
                             Lane Harris- <i>Role</i>
@@ -52,12 +71,19 @@ const AboutUs = (props) => {
                     </div>
                 </div>
                 <div className="link-box">
-                    <i className="fab fa-github"></i>
-                    <i className="fab fa-linkedin"></i>
-                    <i className="fas fa-user-circle"></i>
+                    <a href="https://github.com/lrharris215" target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="https://www.linkedin.com/in/lane-harris-86830320b/" target="_blank">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    {/* personal site goes here */}
+                    <a href="" target="_blank">
+                        <i className="fas fa-user-circle"></i>
+                    </a>
                 </div>
             </div>
-            <div className="about-us-item">
+            <div id="yohan" className="about-us-item">
                 <div className="left">
                     <div className="about-us-pic"></div>
                     <div className="about-us-info">
@@ -68,9 +94,16 @@ const AboutUs = (props) => {
                     </div>
                 </div>
                 <div className="link-box">
-                    <i className="fab fa-github"></i>
-                    <i className="fab fa-linkedin"></i>
-                    <i className="fas fa-user-circle"></i>
+                    <a href="" target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>
+                    <a href="" target="_blank">
+                        <i className="fab fa-linkedin"></i>
+                    </a>
+                    {/* personal site goes here */}
+                    <a href="" target="_blank">
+                        <i className="fas fa-user-circle"></i>
+                    </a>
                 </div>
             </div>
         </div>
