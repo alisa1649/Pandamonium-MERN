d3652855097e3df8780cd27f1c768b59f9aa5a05
Lane Harris <lrharris215@gmail.com>
2021-04-14 11:09:53 -0400

    Started about us skeleton

---
+++ frontend/src/components/about_page/about_us.js
@@ -0,0 +1,72 @@
+const AboutUs = (props) => {
+    return (
+        <div className="about-us-page">
+            <div className="about-us-top">
+                <h2>About Us</h2>
+                <p>
+                    PandaCorp was formed by a group of App Academy Students. Pandamonium is their capstone MERN project
+                    for the semester. Our wonderful team includes:
+                </p>
+            </div>
+
+            <div className="about-us-item">
+                <div className="about-us-pic"></div>
+                <div>
+                    <h3 className="name">
+                        Alisa Cheadle- <i>Role</i>
+                    </h3>
+                    <p className="bio">Write something nice here! What did you work on??</p>
+                </div>
+                <div className="link-box">
+                    <i className="fab fa-github"></i>
+                    <i className="fab fa-linkedin"></i>
+                    <i className="fas fa-user-circle"></i>
+                </div>
+            </div>
+            <div className="about-us-item">
+                <div className="about-us-pic"></div>
+                <div>
+                    <h3 className="name">
+                        Danny Ho- <i>Role</i>
+                    </h3>
+                    <p className="bio">Write something nice here! What did you work on??</p>
+                </div>
+                <div className="link-box">
+                    <i className="fab fa-github"></i>
+                    <i className="fab fa-linkedin"></i>
+                    <i className="fas fa-user-circle"></i>
+                </div>
+            </div>
+            <div className="about-us-item">
+                <div className="about-us-pic"></div>
+                <div>
+                    <h3 className="name">
+                        Lane Harris- <i>Role</i>
+                    </h3>
+                    <p className="bio">Write something nice here! What did you work on??</p>
+                </div>
+                <div className="link-box">
+                    <i className="fab fa-github"></i>
+                    <i className="fab fa-linkedin"></i>
+                    <i className="fas fa-user-circle"></i>
+                </div>
+            </div>
+            <div className="about-us-item">
+                <div className="about-us-pic"></div>
+                <div>
+                    <h3 className="name">
+                        Yohan Oh- <i>Role</i>
+                    </h3>
+                    <p className="bio">Write something nice here! What did you work on??</p>
+                </div>
+                <div className="link-box">
+                    <i className="fab fa-github"></i>
+                    <i className="fab fa-linkedin"></i>
+                    <i className="fas fa-user-circle"></i>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AboutUs;
