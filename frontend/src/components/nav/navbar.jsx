1ded56d3c4c7c7a44dae320a5659fead9d0ba4a5
Lane Harris <lrharris215@gmail.com>
2021-04-14 14:10:07 -0400

    removed extra heading

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -31,18 +31,15 @@ class NavBar extends React.Component {
         return (
             <div className="nav-bar">
                 <div className="left-nav-bar">
-                    <Link to={"/dashboard"} className="heading" >
-
-
+                    <Link to={'/dashboard'} className="heading">
                         <div className="panda">
                             <img src="/panda.png" alt="panda icon" />
                         </div>
                     </Link>
 
-                    <Link to={"/dashboard"} className="heading" ><h1>Pandamonium</h1></Link>
-
-                    <h1>Pandamonium</h1>
-
+                    <Link to={'/dashboard'} className="heading">
+                        <h1>Pandamonium</h1>
+                    </Link>
                 </div>
                 {this.getLinks()}
             </div>
